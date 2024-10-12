341e4735c17f415b2214e662223fb531063da217
skyfoojs <skyfoojs@gmail.com>
2024-10-12 18:18:38 +0800

    Add navbar

---
+++ frontEnd/app/Views/user/components/navBar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../public/css/output.css">
+</head>
+<body>
+    <div class="bg-white flex py-3 px-5 items-center justify-between font-montserrat">
+        <img class="w-40 h-20" src="../../public/images/Logo.png" alt="Logo.png">
+
+        <div class="ml-24 flex font-bold space-x-14 text-lg">
+            <a href="">Home</a>
+            <a href="">Explore</a>
+            <a href="">Nutritionists</a>
+            <a href="">Feedbacks</a>
+        </div>
+
+        <div>
+            <p>Name</p>
+            
+        </div>
+    </div>
+</body>
+</html>
