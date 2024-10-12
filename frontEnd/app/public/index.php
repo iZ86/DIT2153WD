2dae8a2f31468cb3c37d2fd2c4d49817fc8d45d1
skyfoojs <skyfoojs@gmail.com>
2024-10-12 18:13:56 +0800

    Add import statment for userNutritionist.php and add link to output.css

--- frontEnd/app/public/index.php
+++ frontEnd/app/public/index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/output.css">
 </head>
 <body>
     <?php 
-    require_once '../Views/admin/pages/temp.php';
+    require_once '../Views/user/pages/userNutritionists.php';
     ?>
     
 </body>
