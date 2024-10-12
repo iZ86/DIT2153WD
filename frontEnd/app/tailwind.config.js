64d35eb0d1774511eb9ccf55d9597276dce09cb2
skyfoojs <skyfoojs@gmail.com>
2024-10-12 18:20:39 +0800

    Update package and config

--- frontEnd/app/tailwind.config.js
+++ frontEnd/app/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/index.php',
+    './public/**/*.{html,js,php}', // Include all files in public
+    './Views/**/*.{html,js,php}',    // Include all files in Views
+    './src/**/*.{html,js,php}',     // Include all files in src if applicable
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-bg': '#F1FAFF',
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
