2de8c701bc6b6f4e5ae2f988bc643804f7e6a844
skyfoojs <skyfoojs@gmail.com>
2024-10-12 18:19:31 +0800

    Import navbar, header is done, need to adjst wordings

---
+++ frontEnd/app/Views/user/pages/userNutritionists.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../public/output.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body class="bg-white-bg">
+    <?php require_once 'components/navBar.php' ?>
+    
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="mt-12 text-4xl font-bold text-[#02463E] font-montserrat">Meet Our Nutritionists</h1>
+
+        <div class="flex flex-row mt-5">
+            <img src="../../public/images/nutrition_header.png" alt="Nutrition.png">
+            <div class="border-2 border-[#666666] border-solid h-96"></div>
+            <div class="flex flex-col">
+                <p>Why do you need a Nutritionists?</p>
+
+                <p>A nutritionist is essential for creating personalized diet plans that support fitness goals,
+                     prevent chronic diseases, and educate individuals on healthier food choices. They help 
+                     manage special dietary needs, promote overall well-being, and ensure proper nutrition 
+                     complements exercise for better results. In a fitness center, a nutritionist is key to 
+                     achieving balanced health and optimal performance.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
