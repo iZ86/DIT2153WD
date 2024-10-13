a6cb88fd02b2458baa0652e1976f3ed05fe19061
i4GS <admin@i4.gs>
2024-10-13 18:20:15 +0800

    Added navbar.php

---
+++ frontEnd/app/Views/admin/components/navbar.php
@@ -0,0 +1,7 @@
+<main class="flex-1 ml-64">
+    <header class="bg-white shadow p-4 mx-6 mt-5 rounded-3xl flex justify-between items-center">
+        <div class="flex justify-end w-full items-center space-x-4">
+            <span class="font-semibold text-gray-700">Admin</span>
+            <img src="../../../public/images/avatar.png" alt="Admin Avatar" class="w-10 h-10 rounded-full">
+        </div>
+    </header>
