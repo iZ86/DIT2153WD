017b918c3cb53db412403c06c4b9efa1007c0e9a
i4GS <admin@i4.gs>
2024-10-13 18:19:46 +0800

    Added header.php

---
+++ frontEnd/app/Views/admin/components/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard - Huan Fitness Centre</title>
+    <link rel="stylesheet" href="../../../public/css/output.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css">
+
+    <style>
+        body {
+            font-family: 'Montserrat', sans-serif;
+        }
+    </style>
+</head>
+<body class="bg-blue-50 min-h-screen flex flex-col">
