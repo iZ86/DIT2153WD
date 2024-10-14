88fb0ca089eb924946e19b3856ee8dedd6c854c1
i4GS <admin@i4.gs>
2024-10-14 16:18:04 +0800

    Fixed the background color of each menu will change base on the current pages

--- frontEnd/app/Views/admin/components/sidebar.php
+++ frontEnd/app/Views/admin/components/sidebar.php
@@ -4,47 +4,59 @@
             <img src="../../../public/images/Logo.png" alt="Logo" class="w-48 mx-auto">
         </a>
     </div>
+
+    <?php
+    $current_page = basename($_SERVER['PHP_SELF']);
+    ?>
+
     <nav class="px-6 text-gray-700">
         <h2 class="text-gray-500 mt-2 mb-5 px-5 font-medium">Menu</h2>
         <ul class="space-y-4">
             <li>
-                <a href="../../../Views/admin/pages/index.php" class="flex items-center space-x-5 py-3 px-5 bg-black text-white rounded-md">
+                <a href="../../../Views/admin/pages/index.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'index.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bxs-dashboard text-base"></i>
                     <span class="font-medium">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/users.php" class="flex items-center space-x-5 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/users.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'users.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-user text-base"></i>
                     <span class="font-medium">Users</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/classes.php" class="flex items-center space-x-5 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/classes.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'classes.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-book text-base"></i>
                     <span class="font-medium">Classes</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/nutritionists.php" class="flex items-center space-x-5 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/nutritionists.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'nutritionists.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-food-menu text-base"></i>
                     <span class="font-medium">Nutritionists</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/instructors.php" class="flex items-center space-x-5 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/instructors.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'instructors.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-run text-base"></i>
                     <span class="font-medium">Instructors</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/payments.php" class="flex items-center space-x-6 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/payments.php"
+                   class="flex items-center space-x-6 py-3 px-5 <?php echo $current_page == 'payments.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-credit-card text-base"></i>
                     <span class="font-medium">Payments</span>
                 </a>
             </li>
             <li>
-                <a href="../../../Views/admin/pages/feedbacks.php" class="flex items-center space-x-5 py-3 px-5 hover:bg-gray-100 rounded-md">
+                <a href="../../../Views/admin/pages/feedbacks.php"
+                   class="flex items-center space-x-5 py-3 px-5 <?php echo $current_page == 'feedbacks.php' ? 'bg-black text-white' : 'hover:bg-gray-100'; ?> rounded-md">
                     <i class="bx bx-message-square-dots text-base"></i>
                     <span class="font-medium">Feedbacks</span>
                 </a>
