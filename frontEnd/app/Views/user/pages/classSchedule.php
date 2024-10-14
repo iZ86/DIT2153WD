7cc0f4d96dd675adafae3ba845b1bdd738d4b0f1
skyfoojs <skyfoojs@gmail.com>
2024-10-14 19:35:03 +0800

    Add classSchedule.php

---
+++ frontEnd/app/Views/user/pages/classSchedule.php
@@ -0,0 +1,111 @@
+<?php include '../components/header.php'; ?>
+<?php include '../components/navBar.php'; ?>
+
+<section class="bg-[#F1FAFF]">
+    <div class="flex justify-center items-center relative">
+    <i class="bx bx-chevron-left bx-lg ml-20 absolute left-20 top-10"></i>
+        <div class="flex flex-col">
+            <div class="flex flex-col font-bold font-montserrat text-4xl items-center my-5">
+                <p>Yoga Class</p>
+                <p>Instructor: Grace Evans</p>
+            </div>
+
+            <table class="border border-black border-solid text-center">
+                <tr class="bg-[#9C9292] text-white">
+                    <th>Routine</th>
+                    <th>8:00 AM</th>
+                    <th>10:00 AM</th>
+                    <th>1:00 PM</th>
+                    <th>3:00 PM</th>
+                    <th>5:00 PM</th>
+                </tr>
+
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                
+                <tr>
+                    <th class="date">29/9/2024<br>Monday</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+
+            <div class="flex justify-end mt-3 gap-x-5">
+            <i class="bx bx-chevron-left bx-md"></i>
+            <i class="bx bx-chevron-right bx-md"></i>
+            </div>
+        </div>
+    </div>
+</section>
+<style>
+tr, th, td {
+  border: 1px solid #EEEEEE;
+
+  th {
+    padding: 10px 30px;
+  }
+
+  td {
+    padding: 40px 100px;
+  }
+
+  .date {
+    color: white;
+    background-color: black;
+  }
+}
+</style>
