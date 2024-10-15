5ff3db99f465f5b3efb8286d441c8d6069ac2937
i4GS <admin@i4.gs>
2024-10-15 14:17:37 +0800

    Change font family to Nunito

--- frontEnd/app/Views/admin/components/header.php
+++ frontEnd/app/Views/admin/components/header.php
@@ -6,12 +6,13 @@
     <title>Admin Dashboard - Huan Fitness Centre</title>
     <link rel="stylesheet" href="../../../public/css/output.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css">
 
     <style>
         body {
-            font-family: 'Montserrat', sans-serif;
+            font-family: 'Nunito', sans-serif;
         }
     </style>
 </head>
-<body class="bg-blue-50 min-h-screen flex flex-col">
+<body class="bg-indigo-50 min-h-screen flex flex-col">
