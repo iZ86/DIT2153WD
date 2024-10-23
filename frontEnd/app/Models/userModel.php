93d1f49ccefb1d8953abb74c7e9a2207250fe089
iZ86 <iZLikesCS@gmail.com>
2024-10-23 13:19:44 +0800

    Add createUser method

--- frontEnd/app/Models/userModel.php
+++ frontEnd/app/Models/userModel.php
@@ -22,6 +22,53 @@ class UserModel {
             return $row;
         }
         return false;
-        
+    }
+
+    /** Creates user data,
+     * returns 1 if true,
+     * returns 2 if only registered_user data has been added.
+     * returns 3 if only user data has been added.
+     * Otherwise, returns 0 if all data failed to be added.
+     */
+    public function createUser($firstName, $lastName, $username, $password, $email, $phoneNo, $gender, $dateOfBirth, $profileImageFilePath, $joinedDate) {
+
+        // Add Registered_User record.
+        $registeredUserSQL = "INSERT INTO " . $this->registeredUserTable . "(firstName, lastName, username, password, email, phoneNo, gender, dateOfBirth, profileImageFilePath) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $registeredUserSQLSTMT = $this->databaseConn->prepare($registeredUserSQL);
+
+        // Bind parameters.
+        $registeredUserSQLSTMT->bind_param("sssssssss", $firstName, $lastName, $username, $password, $email, $phoneNo, $gender, $dateOfBirth, $profileImageFilePath);
+
+        // Execute the statement.
+        $insertRegisteredUserStatus = $registeredUserSQLSTMT->execute();
+
+
+        // If added Registered_User record completed.
+        // Add User record.
+
+        $insertUserStatus;
+
+        if ($insertRegisteredUserStatus) {
+            $userId = $this->databaseConn->insert_id;
+            $userSQL = "INSERT INTO " . $this->userTable . " VALUES (?, ?)";
+            $userSQLSTMT = $this->databaseConn->prepare($userSQL);
+
+            // Bind parameters.
+            $userSQLSTMT->bind_param("ss", $userId, $joinedDate);
+
+            // Execute the statement.
+            $insertUserStatus = $userSQLSTMT->execute();
+        }
+
+        if ($insertRegisteredUserStatus && $insertUserStatus) {
+            return 1; // Both registered_user and user data has been added.
+        } else if ($insertRegisteredUserStatus && !$insertUserStatus) {
+            return 2; // Only registered_user data has been added.
+        } else if (!$insertRegisteredUserStatus && $insertUserStatus) {
+            return 3; // Only user data has been added.
+        } else {
+            return 0; // Both data has failed to been added.
+        }
+
     }
 }
