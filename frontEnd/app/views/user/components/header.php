9ff03fc453ac4935f07842af19a071c2c01f3efa
iZ86 <iZLikesCS@gmail.com>
2024-10-24 17:02:56 +0800

    Modify href for stylesheet
    now all controllers can access

--- frontEnd/app/views/user/components/header.php
+++ frontEnd/app/views/user/components/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Huan Fitness Center - Website</title>
-    <link rel="stylesheet" href="../public/css/output.css">
+    <link rel="stylesheet" href=<?php echo '"' . preg_replace("/.+\/xampp\/htdocs\//", "http://localhost/", str_replace('\\', '/', __DIR__)) . "/../../../public/css/output.css" . '"'?>>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -13,3 +13,4 @@
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css">
 </head>
 <body>
+
