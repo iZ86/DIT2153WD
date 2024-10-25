7d869f5a77b34d567dc66cd5486e23d606c3582b
iZ86 <iZLikesCS@gmail.com>
2024-10-25 16:33:26 +0800

    Modify gaps between components
    more nicer

--- frontEnd/app/views/user/components/navBar.php
+++ frontEnd/app/views/user/components/navBar.php
@@ -1,17 +1,22 @@
 <nav>
-    <div class="bg-white flex py-3 px-5 items-center justify-between font-montserrat">
+    <div class="bg-white flex py-3 px-12 font-montserrat flex justify-between">
         <img class="w-40 h-20" src="../../public/images/Logo.png" alt="Logo.png">
+        
+        <div class="px-11 flex items-center gap-x-14">
+            <div class="flex font-bold space-x-14 text-lg">
+                <a href="">Home</a>
+                <a href="">Explore</a>
+                <a href="">Nutritionists</a>
+                <a href="">Feedbacks</a>
+            </div>
 
-        <div class="ml-24 flex font-bold space-x-14 text-lg">
-            <a href="">Home</a>
-            <a href="">Explore</a>
-            <a href="">Nutritionists</a>
-            <a href="">Feedbacks</a>
+            <div class="flex justify-center items-center">
+                <p>Name</p>
+                <div class="border border-black border-solid rounded-full w-10 h-10 ml-4"></div>
+            </div>
+            
         </div>
 
-        <div class="flex justify-center items-center">
-            <p>Name</p>
-            <div class="border border-black border-solid rounded-full w-10 h-10 ml-5"></div>
-        </div>
+        
     </div>
 </nav>
