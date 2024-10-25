0cdcbabe8a341c0263c92e05929ef6f8ca113a18
iZ86 <iZLikesCS@gmail.com>
2024-10-25 15:45:10 +0800

    Modify indexView to intial code

--- frontEnd/app/views/user/pages/indexView.php
+++ frontEnd/app/views/user/pages/indexView.php
@@ -1,2 +1,29 @@
-<?php include '../components/header.php'; ?>
-<?php include '../components/navbarLoggedOut.php'; ?>
+<?php
+
+class IndexView {
+    // Constructor for the object
+    public function __construction() {
+
+    }
+
+    /** Renders the whole view. */
+    public function renderView() : void {
+        $this->renderHeader();
+        $this->renderNavbar();
+    }
+
+    /** Renders the header of the view. */
+    public function renderHeader() : void {
+        include __DIR__ . '/../components/header.php';
+    }
+
+    /** Renders the navbar. */
+    public function renderNavbar() : void {
+        include __DIR__ . '/../components/navbar.php';
+    }
+
+    /** Renders the content. */
+    public function renderContent(): void {
+    }
+
+}
