6d9ff223fa9ecf6c63f14faf9ba17b7c5685e25a
iZ86 <iZLikesCS@gmail.com>
2024-10-26 23:59:20 +0800

    Add getWaterConsumptionDataInDays($userID, $startDate, $endDate)

--- frontEnd/app/models/user/userTrackWaterConsumptionModel.php
+++ frontEnd/app/models/user/userTrackWaterConsumptionModel.php
@@ -10,5 +10,20 @@ class UserTrackWaterConsumptionModel {
         $this->databaseConn = $databaseConn;
     }
 
-
+    /** Returns an array of water consumption data from $startDateTime to $endDateTime. */
+    public function getWaterConsumptionDataInDays($userID, $startDateTime, $endDateTime) {
+        $waterConsumptionSQL = "SELECT * FROM " .
+        $this->waterConsumptionTable .
+        " WHERE userID = ? AND recordedOn BETWEEN ? AND ? ORDER BY recordedOn DESC";
+        
+        $waterConsumptionSTMT = $this->databaseConn->prepare($waterConsumptionSQL);
+        $waterConsumptionSTMT->bind_param("sss", $userID, $startDateTime, $endDateTime);
+        $waterConsumptionSTMT->execute();
+        $waterConsumptionResult = $waterConsumptionSTMT->get_result();
+        $waterConsumptionResultArray = array();
+        for ($i = 0; $i < $waterConsumptionResult->num_rows; $i++) {
+            $waterConsumptionResultArray[] = $waterConsumptionResult->fetch_assoc();
+        }
+        return $waterConsumptionResultArray;
+    }
 }
