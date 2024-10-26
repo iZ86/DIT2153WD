a509e2664f33ab3d62912bc9a97aa2d1fbcb50c7
iZ86 <iZLikesCS@gmail.com>
2024-10-26 13:55:29 +0800

    Add verifying login credentials and log to user index

--- frontEnd/app/controllers/login.php
+++ frontEnd/app/controllers/login.php
@@ -1,7 +1,8 @@
 <?php
 require "../views/guest/pages/guestLogInView.php";
-
+require "../models/guest/guestLogInModel.php";
 session_start();
+$guestLogInModel = new GuestLogInModel(require "../config/db_connection.php");
 $guestLogInView = new GuestLogInView();
 $guestLogInView->renderView();
 unset($_SESSION['invalidLogin']);
@@ -13,11 +14,17 @@ if (isset($_POST['loginButton']) && $_POST['loginButton'] === "Log In") {
         $_SESSION['invalidLogin'] = 1;
         die(header("location: ". $_SERVER['PHP_SELF']));
     } else {
-
+        if ($guestLogInModel->verifyLogInCredentials($_POST['username'], $_POST['password'])) {
+            // If it's not a user account, its an admin account.
+            // No need to do extra checks, until future more usertypes.
+            if ($guestLogInModel->isUserAccount($_POST['username'])) {
+                die(header("location: http://localhost/DIT2153WD/frontEnd/app/controllers/user/"));
+            } else {
+                // TODO: head to admin controller
+            }
+        }
     }
     
-} else {
-    // Go to index.php of user.
 }
 
 
