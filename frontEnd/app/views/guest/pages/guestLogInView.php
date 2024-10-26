30901bd5ad3c5ff46a9e52663d9aab32f542f1ee
iZ86 <iZLikesCS@gmail.com>
2024-10-26 12:58:04 +0800

    Add invalidLogin prompt

--- frontEnd/app/views/guest/pages/guestLogInView.php
+++ frontEnd/app/views/guest/pages/guestLogInView.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 
 class GuestLogInView {
     /** Constructor for the object. */
     public function __construction() {
-
     }
 
     /** Renders the whole view. */
@@ -41,6 +40,7 @@ class GuestLogInView {
             <img src="../public/images/Logo.png" class="w-64 h-32 mb-12">
             <!-- This object is only called at controllers/login.php, so the PHP_SELF will reference the login.php. -->
             <form class="flex flex-col mb-8 w-full" method="post" action=<?php echo $_SERVER['PHP_SELF']?>>
+                
                 <label class="text-xl font-bold">Username</label>
                 <input type="text" name="username" class="bg-slate-100 rounded py-1 px-2 mb-6">
 
@@ -52,6 +52,13 @@ class GuestLogInView {
                     <label class="font-semibold text-gray-dove text-base">Keep me logged in</label>
                 </div>
 
+                <!-- Invalid login warning -->
+                <?php 
+                if (isset($_SESSION['invalidLogin'])) {?>
+                <label class="text-red-600 mx-auto">Invalid username/password</label>
+                <?php
+                }
+                ?>
                 <input type="submit" name="loginButton" value="Log In" class="bg-blue-light hover:bg-blue-neon px-8 py-4 mx-auto rounded-lg text-white text-sm font-bold">
 
             </form>
@@ -68,3 +75,4 @@ class GuestLogInView {
     <?php
     }
 }
+
