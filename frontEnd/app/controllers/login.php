fff041dd460dcd7fee05eb727ea0f6a7ce478d00
iZ86 <iZLikesCS@gmail.com>
2024-10-26 12:57:23 +0800

    Add validation for empty username and password

--- frontEnd/app/controllers/login.php
+++ frontEnd/app/controllers/login.php
@@ -1,4 +1,23 @@
 <?php
 require "../views/guest/pages/guestLogInView.php";
+
+session_start();
 $guestLogInView = new GuestLogInView();
 $guestLogInView->renderView();
+unset($_SESSION['invalidLogin']);
+
+
+if (isset($_POST['loginButton']) && $_POST['loginButton'] === "Log In") {
+
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        $_SESSION['invalidLogin'] = 1;
+        die(header("location: ". $_SERVER['PHP_SELF']));
+    } else {
+
+    }
+    
+} else {
+    // Go to index.php of user.
+}
+
+
