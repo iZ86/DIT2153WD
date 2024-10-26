138122fe6c7fe0cede67e10e56f57c6fd1f8fe7e
iZ86 <iZLikesCS@gmail.com>
2024-10-26 22:14:31 +0800

    Add renderContent() intitial code

--- frontEnd/app/views/user/pages/userTrackWaterConsumptionView.php
+++ frontEnd/app/views/user/pages/userTrackWaterConsumptionView.php
@@ -34,6 +34,44 @@ class UserTrackWaterConsumptionView {
 
     /** Renders the content. */
     public function renderContent(): void {?>
+    <section class="font-montserrat bg-blue-user flex flex-col pt-16 pb-48">
+        <h1 class="text-4xl font-bold ml-20 mb-14">Track Water Consumption</h1>
+
+        <div class="mx-20 basis-64 bg-blue-vivid rounded-2xl flex flex-col items-center mb-32">
+            <img src="../../public/images/track-water-consumption-icon.png" class="w-16 h-16 mb-10" style="margin-top: 20px;">
+            <div class="text-white font-bold text-3xl drop-shadow-dark">
+                <p class="mb-0">Yo have drank [5L] today!</p>
+                <p class="mb-0">[Keep up the good work!]</p>
+            </div>
+        </div>
+
+        <!-- Today Track -->
+        <div class="flex flex-col">
+            <div class="flex">
+                <h2 class="text-4xl font-bold ml-20">Today</h2>
+                <input type="button" id="addNewWaterConsumptionData" value="+">
+            </div>
+
+            <div class="flex items-center justify-between mt-14 mx-24">
+
+
+                <input type="button" id="previousFeedback" name="previousFeedback" value="<" class="w-8 h-20 text-4xl font-bold bg-gray-200">
+
+                <!-- Feedback card layout -->
+                <div class="flex items-center">
+
+                    
+                    <div class="w-64 h-64 bg-purple-100 rounded-2xl">
+                    </div>
+                    
+                    
+                </div>
+
+                <input type="button" id="nextFeedback" name="nextFeedback" value=">" class="w-8 h-20 text-4xl font-bold bg-gray-200">
+            </div>
+        </div>
+
+    </section>
 
 
 
