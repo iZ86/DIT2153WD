e3297eb88fee9004d126cb3e0354d62e06b81b55
iZ86 <iZLikesCS@gmail.com>
2024-10-26 09:37:04 +0800

    Fix wrong regex for img logo src

--- frontEnd/app/views/guest/pages/guestLogInView.php
+++ frontEnd/app/views/guest/pages/guestLogInView.php
@@ -38,7 +38,7 @@ class GuestLogInView {
         
         <div class="bg-white py-11 px-72 rounded-3xl shadow-lg mx-auto flex flex-col items-center">
             <!-- This code is used as this class is called by other php file. Therefore, to not keep track of the references, this code is used -->
-            <img src=<?php echo '"' . preg_replace("/.+\/xampp\/htdocs\//", "http://localhost/", str_replace('\\', '/', __DIR__)) . "/../../../public/images/Logo.png" . '"'?> class="w-64 h-32 mb-12">
+            <img src=<?php echo '"' . preg_replace("/.+\/DIT2153WD\//", "http://localhost/DIT2153WD/", str_replace('\\', '/', __DIR__)) . "/../../../public/images/Logo.png" . '"'?> class="w-64 h-32 mb-12">
             <form class="flex flex-col mb-8 w-full">
                 <label class="text-xl font-bold">Username</label>
                 <input type="text" id="username" name="username" class="bg-slate-100 rounded py-1 px-2 mb-6">
