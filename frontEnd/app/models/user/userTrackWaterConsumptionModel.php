26a3909d4ea9b78d86fd6857e11dc24b11205cd4
iZ86 <iZLikesCS@gmail.com>
2024-10-26 23:43:52 +0800

    Add missing semi colon

--- frontEnd/app/models/user/userTrackWaterConsumptionModel.php
+++ frontEnd/app/models/user/userTrackWaterConsumptionModel.php
@@ -3,7 +3,7 @@ class UserTrackWaterConsumptionModel {
     /** Database connection. */
     private $databaseConn;
     /** Water_consumption table */
-    private $waterConsumptionTable = "WATER_CONSUMPTION"
+    private $waterConsumptionTable = "WATER_CONSUMPTION";
 
     /** Constructor for model. */
     public function __construct($databaseConn) {
