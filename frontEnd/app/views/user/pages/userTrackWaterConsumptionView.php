82c8ce7bb0fa16ea8b9f9043f3c164f41adeb660
iZ86 <iZLikesCS@gmail.com>
2024-10-26 20:12:23 +0800

    Add userTrackWaterConsumptionView.php initial commit

---
+++ frontEnd/app/views/user/pages/userTrackWaterConsumptionView.php
@@ -0,0 +1,42 @@
+<?php
+
+class UserTrackWaterConsumptionView {
+    
+    /** Constructor for the object. */
+    public function __construct() {
+    }
+
+    /** Renders the whole view. */
+    public function renderView() : void {
+        $this->renderHeader();
+        $this->renderNavbar();
+        $this->renderContent();
+        $this->renderFooter();
+    }
+
+    /** Renders the header. */
+    public function renderHeader() : void {
+        // Use __DIR__ to prevent referencing issues, as this object is called by other php files.
+        include __DIR__ . '/../components/userHeader.php';
+    }
+
+    /** Renders the navbar. */
+    public function renderNavbar() : void {
+        // Use __DIR__ to prevent referencing issues, as this object is called by other php files.
+        include __DIR__ . '/../components/userNavbar.php';
+    }
+
+    /** Renders the footer. */
+    public function renderFooter() : void {
+        // Use __DIR__ to prevent referencing issues, as this object is called by other php files.
+        include __DIR__ . '/../components/userFooter.php';
+    }
+
+    /** Renders the content. */
+    public function renderContent(): void {?>
+
+
+
+    <?php
+    }
+}
