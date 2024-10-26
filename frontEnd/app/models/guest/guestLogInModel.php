41070fd7ee22ddc8387834db014199c91c2fee0d
iZ86 <iZLikesCS@gmail.com>
2024-10-26 13:33:17 +0800

    Add verifLogInCredentials($username, $password)

--- frontEnd/app/models/guest/guestLogInModel.php
+++ frontEnd/app/models/guest/guestLogInModel.php
@@ -14,6 +14,28 @@ class GuestLogInModel {
         $this->databaseConn = $databaseConn;
     }
 
-    
+    /** Verify login credentials.
+     * Returns 1, if the login is valid.
+     * Otherwise, returns 0.
+     */
+    public function verifyLogInCredentials($username, $password) {
+        $registeredUserSQL = "SELECT * FROM " . $this->registeredUserTable . " WHERE username = ?";
+        $registeredUserSTMT = $this->databaseConn->prepare($registeredUserSQL);
+        $registeredUserSTMT->bind_param("s", $username);
+        $registeredUserSTMT->execute();
+        $registeredUserResult = $registeredUserSTMT->get_result();
+        if ($registeredUserResult->num_rows > 0) {
+            
+            $registeredUserRow = $registeredUserResult->fetch_assoc();
+
+            // TODO: Modify password verification to include hashing.
+
+            if ($registeredUserRow['password'] === $password) {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
 
 }
