2c69d888dd06e9b71348ad65c8e6f6e4ea05ecfc
iZ86 <iZLikesCS@gmail.com>
2024-10-26 13:02:58 +0800

    Add guestLogInModel.php intial code

---
+++ frontEnd/app/models/guest/guestLogInModel.php
@@ -0,0 +1,19 @@
+<?php
+class GuestLogInModel {
+    /** Database connection. */
+    private $databaseConn;
+    /** Registered_User table */
+    private $registeredUserTable = "REGISTERED_USER";
+    /** User table. */
+    private $userTable = "USER"
+    /** Admin table. */
+    private $adminTable = "ADMIN";
+
+    /** Constructor for model. */
+    public function __construct($databaseConn) {
+        $this->$databaseConn;
+    }
+
+    
+
+}
