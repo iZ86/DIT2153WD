5c71063e346cad97d5df665344cb3e46a775ac13
skyfoojs <skyfoojs@gmail.com>
2024-10-26 14:10:00 +0800

    Create getBookingInformation() - Retrieve booking information by using  from view

--- frontEnd/app/controllers/user/nutritionist.php
+++ frontEnd/app/controllers/user/nutritionist.php
@@ -3,12 +3,35 @@ require '../../models/nutritionistModel.php';
 require '../../views/user/pages/userNutritionistsView.php';
 
 $nutritionistModel = new NutritionistModel(require '../../config/db_connection.php');
+
+/** Retrieve booking information from view by using $_POST.
+ * Use ?? to provide a default null value, if the $_POST doesn't retrieve it. 
+ */
+function getBookingInformation() {
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['desc'])) {
+        $nutritionist = $_POST['nutritionist'] ?? null;
+        $date = $_POST['date'] ?? null;
+        $time = $_POST['time'] ?? null;
+        $description = $_POST['desc'] ?? null;
+        $makeReservation = isset($_POST['make-reservation']);
+         
+        // Test if it can retreive the data.
+        echo $nutritionist . $date . $time . $description . $makeReservation;
+        /** Ensure required fields are not empty. */
+        if ($nutritionist && $date && $time) {
+            global $nutritionistModel;
+            $nutritionistModel->nutritionistsBookingHandler(
+                $nutritionist, 
+                date('Y-m-d', strtotime($date)), 
+                $time, 
+                $description, 
+                $makeReservation
+            );
+        }
+    }
+}
+
+/** Fetch nutritionists for display in the view. */
 $nutritionistsView = new NutritionistsView($nutritionistModel->getAllNutritionist());
-
 $nutritionistsView->renderView();
-
-/** Function to return a nutritionist by their ID. */
-function getNutritionistById(int $id) { 
-    global $nutritionistModel; 
-    return $nutritionistModel->getById($id);
-} 
+getBookingInformation();
