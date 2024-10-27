5dfd08a9c28803a5dc7154eefb62f3c428853b07
iZ86 <iZLikesCS@gmail.com>
2024-10-27 12:50:13 +0800

    Add data retrieval and pagination

--- frontEnd/app/controllers/user/track-water-consumption.php
+++ frontEnd/app/controllers/user/track-water-consumption.php
@@ -1,4 +1,18 @@
 <?php
 require('../../views/user/pages/userTrackWaterConsumptionView.php');
-$userTrackWaterConsumptionView = new UserTrackWaterConsumptionView();
+require('../../models/user/userTrackWaterConsumptionModel.php');
+session_start();
+$userTrackWaterConsumptionModel = new UserTrackWaterConsumptionModel(require "../../config/db_connection.php");
+
+$regex = "/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/";
+
+
+if (!(isset($_GET['date'])) || !preg_match($regex, $_GET['date']) || (date($_GET['date']) > date("Y-m-d"))) {
+    die(header('location: http://localhost/DIT2153WD/frontEnd/app/controllers/user/track-water-consumption.php?date=' . date("Y-m-d")));
+}
+
+$date = $_GET['date'];
+
+$userTrackWaterConsumptionView = new UserTrackWaterConsumptionView($userTrackWaterConsumptionModel->getWaterConsumptionDataFromDate($_SESSION['userID'], $date));
 $userTrackWaterConsumptionView->renderView();
+
