5508906700061aa8533b4e86ea6b7750bdbc68cd
iZ86 <iZLikesCS@gmail.com>
2024-10-27 19:23:43 +0800

    Add comments

--- frontEnd/app/public/js/user/userTrackWaterConsumptionScript.js
+++ frontEnd/app/public/js/user/userTrackWaterConsumptionScript.js
@@ -1,8 +1,10 @@
+/** Redirects the user based on the date inputted in the calendar. */
 function redirectTrackWaterConsumptionPage() {
     let dateInput = document.getElementById("dateOfWaterConsumption");
     location.href = "http://localhost/DIT2153WD/frontEnd/app/controllers/user/track-water-consumption.php?date=" + dateInput.value;
 }
 
+/** Redirects the user to the same website with GET request to the previous date. */
 function previousDate() {
     let dateInput = document.getElementById("dateOfWaterConsumption");
     let date = new Date(dateInput.value);
@@ -10,6 +12,7 @@ function previousDate() {
     location.href = "http://localhost/DIT2153WD/frontEnd/app/controllers/user/track-water-consumption.php?date=" + date.getFullYear() + "-" + (date.getMonth() +  1)+ "-" + date.getDate();
 }
 
+/** Redirects the user to the same website with GET request to the next date. */
 function nextDate() {
     let dateInput = document.getElementById("dateOfWaterConsumption");
     let date = new Date(dateInput.value);
