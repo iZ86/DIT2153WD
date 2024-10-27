9e52cdea823f2d4035a429a43d0c290d8eba8519
i4GS <admin@i4.gs>
2024-10-27 22:23:13 +0800

    Done with some basic function in adminIndex controller

--- frontEnd/app/controllers/admin/index.php
+++ frontEnd/app/controllers/admin/index.php
@@ -1,4 +1,14 @@
 <?php
 require '../../views/admin/pages/adminIndexView.php';
-$adminIndexView = new AdminIndexView();
+require '../../models/admin/adminIndexModel.php';
+session_start();
+
+$adminIndexModel = new AdminIndexModel(require '../../config/db_connection.php');
+
+$usersCount = $adminIndexModel->countUsers();
+$classesCount = $adminIndexModel->countClasses();
+$nutritionistsCount = $adminIndexModel->countNutritionists();
+$instructorsCount = $adminIndexModel->countInstructors();
+
+$adminIndexView = new AdminIndexView($usersCount, $classesCount, $nutritionistsCount, $instructorsCount);
 $adminIndexView->renderView();
