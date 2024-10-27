1ebbaa2de1809d8b01a00dad19a9dba757577e4d
skyfoojs <skyfoojs@gmail.com>
2024-10-27 19:24:57 +0800

    Create controller for fitness class schedule - Use to retrieve instructor, week information from URL - pass fitness class information and instructor name

---
+++ frontEnd/app/controllers/user/fitness-class.php
@@ -0,0 +1,17 @@
+<?php
+require '../../models/user/userFitnessClassModel.php';
+require '../../views/user/pages/classScheduleView.php';
+
+// Assuming a request handling script
+$instructorId = isset($_GET['instructor']) ? intval($_GET['instructor']) : null;
+$weekOffset = isset($_GET['week']) ? intval($_GET['week']) : 0;
+
+$fitnessClassModel = new UserFitnessClass(require '../../config/db_connection.php');
+
+// Fetch instructor name and class data
+$instructorName = $fitnessClassModel->getInstructorNameById($instructorId);
+$classData = $fitnessClassModel->getClassesByInstructorById($instructorId);
+
+// Pass the data to the view
+$fitnessClassView = new FitnessClassView($classData, $instructorName);
+$fitnessClassView->renderView();
