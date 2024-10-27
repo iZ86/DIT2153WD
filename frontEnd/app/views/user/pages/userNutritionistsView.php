ea5b93c1f1cfd5c9ddbe9c72f0aa7cc10aa5fd4f
skyfoojs <skyfoojs@gmail.com>
2024-10-27 16:57:00 +0800

    Remove modal for bootstrap, add tailwindcss modal

--- frontEnd/app/views/user/pages/userNutritionistsView.php
+++ frontEnd/app/views/user/pages/userNutritionistsView.php
@@ -31,35 +31,75 @@ class NutritionistsView {
             }
     }
 
-    public function renderBookingConfirmationOverlay() {
-    ?>
-        
-    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-        <div class="modal-body flex flex-col justify-center items-center font-montserrat">
-            <i class="bx bx-check-circle bx-lg bg-green-500" style="color: #22c55e;"></i>
-            <p class="text-xl font-bold mt-3">Booking Confirmed</p>
-            <p>Thank you for choosing our Nutritionist!</p>
-            <p>We will provide you the best service</p>
-            <button type="button" class="text-red-500" data-bs-dismiss="modal"><u>Close Window</u></button>
-        </div>
-        </div>
-    </div>
-    </div>
-    <?php
+    /** Renders the navbar. */
+    public function renderNavbar() {
+        include __DIR__ . '/../components/userNavbar.php';
     }
 
-    //TODO: Fix Modal ignoring the validation, straight change to confirm booking overlay.
-    public function renderBookingFormOverlay() {
-    ?>
-    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    /** Renders the header of the view. */
+    public function renderHeader() {
+        include __DIR__ . '/../components/userHeader.php';
+
+    }
+
+    /** Reners the footer */
+    public function renderFooter() {
+        include __DIR__ . '/../components/userHeader.php';
+    }
+
+    /** Renders the content */
+    public function renderContent() { ?>
+    <section class="bg-white-bg">
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="mt-12 text-4xl font-bold text-[#02463E] font-montserrat">Meet Our Nutritionists</h1>
+
+        <div class="flex items-center justify-center gap-x-10 mt-10">
+            <img class="w-3/4 h-72" src="../../public/images/nutrition_header.png" alt="Nutrition.png">
+            <div class="border border-[#666666] border-solid h-[17rem]"></div>
+            <div class="flex flex-col">
+                <p class="font-bold text-[#00796B] font-montserrat text-xl">Why do you need a Nutritionist?</p>
+                <br>
+                <p class="text-sm text-[#00796B] font-montserrat w-96 leading-6">
+                    A nutritionist is essential for creating personalized diet plans that support fitness goals,
+                    prevent chronic diseases, and educate individuals on healthier food choices. They help
+                    manage special dietary needs, promote overall well-being, and ensure proper nutrition
+                    complements exercise for better results. In a fitness center, a nutritionist is key to
+                    achieving balanced health and optimal performance.
+                </p>
+            </div>
         </div>
-        <div class="modal-body">   
+        <button onclick="openModal()" class="bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center space-x-2">
+            <span>Book a Nutritionist</span>
+        </button>
+
+
+        <div class="bg-white mt-32 flex flex-col items-center justify-center">
+            <div class="flex flex-col">
+                <div class="mx-20 pl-10 py-10 flex border border-black border-solid rounded-lg shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
+                    <div class="bg-[#ECECEC] w-96 h-48 rounded-2xl flex justify-center items-center">
+                        <img src="../../public/images/emily_nutritionist.png" alt="Nutritionist.png">
+                    </div>
+
+                    <div class="ml-10 font-montserrat">
+                        <?=
+                        /** Function that calls the renderNutritionists() function to show all the nutritionists. */
+                        $this->renderNutritionists(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div id="modalOverlay" class="fixed inset-0 bg-black bg-opacity-50 hidden z-40"></div>
+
+<div id="userModal" class="fixed inset-0 flex items-center justify-center hidden z-50 modal">
+    <div class="bg-white w-full max-w-lg rounded-2xl shadow-lg p-6 mx-4 modal-content">
+        <div class="flex justify-between items-center text-center pb-4">
+        <h2 id="modalTitle" class="text-2xl font-semibold">Make Reservation</h2>
+        <button type="button" onclick="closeModal()" class="text-4xl font-bold">&times;</button>
+        </div>
+        <hr class="py-2">
         <form class="flex flex-col gap-y-5 mt-3 pb-3 w-full justify-center items-center" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
             <div>
                 <label class="font-nunito" for="nutritionist">Nutritionist:</label><br>
@@ -80,7 +120,7 @@ class NutritionistsView {
                         echo '<option value="" disabled>No nutritionists available</option>';
                     }*/
                     ?>
-                </select> 
+                </select>
             </div>
 
             <div>
@@ -93,86 +133,21 @@ class NutritionistsView {
                 <select required class="w-72 border-b-[1px] border-black border-solid" name="time" id="time" placeholder="SELECT TIME">
                     <option value="" disabled selected hidden>SELECT AN AVAILABLE TIME</option>
                     <option value="hi" >Hi</option>
-                </select> 
+                </select>
             </div>
 
             <div>
                 <label class="font-nunito" for="desc">Description:</label><br>
-                <textarea class="pl-3 border-[1px] border-black border-solid rounded-md font-nunito resize-none" name="desc" id="desc" rows="4" cols="32" placeholder="Please Tell Us Your GOAL or Any Concern"></textarea> 
+                <textarea class="pl-3 border-[1px] border-black border-solid rounded-md font-nunito resize-none" name="desc" id="desc" rows="4" cols="32" placeholder="Please Tell Us Your GOAL or Any Concern"></textarea>
             </div>
 
             <div class="text-center">
                 <p class="font-bold text-sm font-nunito">RM20 for each Consultation Session*</p>
             </div>
-            <button type="submit" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Confirm Booking</button>
+            <button onclick="openConfirmBookingModal()" type="submit" class="bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium py-2 px-4 rounded-lg flex items-center space-x-2">Confirm Booking</button>
         </form>
-        </div>
-        </div>
-        </div>
     </div>
-
-    <button name="make-reservation"  class="btn btn-primary mt-3 font-montserrat" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Make a Reservation</button>
-    <?php
-    }
-
-    /** Renders the navbar. */
-    public function renderNavbar() {
-        include __DIR__ . '/../components/userNavbar.php';
-    }
-    
-    /** Renders the header of the view. */
-    public function renderHeader() {
-        include __DIR__ . '/../components/userHeader.php';
-
-    }
-
-    /** Reners the footer */
-    public function renderFooter() {
-        include __DIR__ . '/../components/userHeader.php';
-    }
-
-    /** Renders the content */
-    public function renderContent() {
-        ?>
-        <section class="bg-white-bg">
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="mt-12 text-4xl font-bold text-[#02463E] font-montserrat">Meet Our Nutritionists</h1>
-
-        <div class="flex items-center justify-center gap-x-10 mt-10">
-            <img class="w-3/4 h-72" src="../../public/images/nutrition_header.png" alt="Nutrition.png">
-            <div class="border border-[#666666] border-solid h-[17rem]"></div>
-            <div class="flex flex-col">
-                <p class="font-bold text-[#00796B] font-montserrat text-xl">Why do you need a Nutritionist?</p>
-                <br>
-                <p class="text-sm text-[#00796B] font-montserrat w-96 leading-6">
-                    A nutritionist is essential for creating personalized diet plans that support fitness goals,
-                    prevent chronic diseases, and educate individuals on healthier food choices. They help 
-                    manage special dietary needs, promote overall well-being, and ensure proper nutrition 
-                    complements exercise for better results. In a fitness center, a nutritionist is key to 
-                    achieving balanced health and optimal performance.
-                </p>
-            </div>
-        </div>
-        <?= $this->renderBookingFormOverlay(); ?>
-
-        <div class="bg-white mt-32 flex flex-col items-center justify-center">
-            <div class="flex flex-col">
-                <div class="mx-20 pl-10 py-10 flex border border-black border-solid rounded-lg shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
-                    <div class="bg-[#ECECEC] w-96 h-48 rounded-2xl flex justify-center items-center">
-                        <img src="../../public/images/emily_nutritionist.png" alt="Nutritionist.png">
-                    </div>
-
-                    <div class="ml-10 font-montserrat">
-                        <?=
-                        /** Function that calls the renderNutritionists() function to show all the nutritionists. */ 
-                        $this->renderNutritionists(); ?> 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
+</div>
 <style>
 select {
     color: rgba(0, 0, 0, 0.4); /* Default color when nothing is selected */
@@ -199,12 +174,50 @@ input[type="date"] {
 input[type="date"]:valid {
     color: black; /* Black color when a valid date is selected */
 }
+
+.modal {
+    transition: opacity 0.3s ease, transform 0.3s ease;
+    opacity: 0;
+    transform: scale(0.9);
+    pointer-events: none;
+}
+
+.modal.show {
+    opacity: 1;
+    transform: scale(1);
+    pointer-events: auto;
+}
+
 </style>
 <script>
+function openModal() {
+    const modal = document.getElementById('userModal');
+    const overlay = document.getElementById('modalOverlay');
 
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 10);
+}
+
+function closeModal() {
+    const modal = document.getElementById('userModal');
+    const overlay = document.getElementById('modalOverlay');
+    let nutritionist = document.getElementsById('nutritionist');
+    let date = document.getElementById('date');
+    let time = document.getElementById('time');
+
+    modal.classList.remove('show');
+    setTimeout(() => {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }, 300);
+}
 </script>
 
-    <?php
-    }
+<?php
+}
 }
 ?>
