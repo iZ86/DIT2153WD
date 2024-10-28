62732858921844f7e44030fcb1fb54cb832d6a57
skyfoojs <skyfoojs@gmail.com>
2024-10-28 12:39:32 +0800

    Add if condition to verify the status of nutritionistID and date - Check if nutritionistID is null, if not null, return the available date back to view - Check if nutritionistID and date is null, if not null, return the available time back to view Comment currently unuse code - comment if statement to insert createNutritionistBooking

--- frontEnd/app/controllers/user/nutritionist.php
+++ frontEnd/app/controllers/user/nutritionist.php
@@ -1,9 +1,19 @@
 <?php
 require '../../models/nutritionistModel.php';
 require '../../views/user/pages/userNutritionistsView.php';
-
+require '../../config/db_connection.php';
 $nutritionistModel = new NutritionistModel(require '../../config/db_connection.php');
+$nutritionistAvailableDate = [];
+$nutritionistAvailableTime = [];
+if (isset($_POST['nutritionistID'])) {
+    $nutritionistID = $_POST['nutritionistID'];
+    $nutritionistAvailableDate = $nutritionistModel->getAllNutritionistAvailableDateById($nutritionistID);
+}
 
+if(isset($_POST['date']) && isset($_POST['nutritionistID'])) {
+    $nutritionistID = $_POST['nutritionistID'];
+    $nutritionistAvailableTime = $nutritionistModel->getAllNutritionistAvailableTimeById($nutritionistID);
+}
 /** Retrieve booking information from view by using $_POST.
  * Use ?? to provide a default null value, if the $_POST doesn't retrieve it.
  */
@@ -16,18 +26,21 @@ function getBookingInformation() {
 
         if (!empty($nutritionist) && !empty($date) && !empty($time)) {
             global $nutritionistModel;
-
+            echo $nutritionist . $description . $date . $time;
             // Create the booking
+            /*
             if ($nutritionistModel->createNutritionistBooking(
                 $nutritionist,
                 $description,
                 date('Y-m-d', strtotime($date)),
                 $time
             )) {
+
                 echo "<script>alert('Successfully Made a Reservation!!');</script>";
             } else {
                 echo "<script>alert('Failed to Make a Reservation. Please try again.');</script>";
             }
+            */
         } else {
             echo "<script>alert('Please fill in all required fields.');</script>";
         }
@@ -36,6 +49,6 @@ function getBookingInformation() {
 
 
 /** Fetch nutritionists for display in the view. */
-$nutritionistsView = new NutritionistsView($nutritionistModel->getAllNutritionist(), $nutritionistModel->getAllNutritionistScheduleInformation());
+$nutritionistsView = new NutritionistsView($nutritionistModel->getAllNutritionist(), $nutritionistAvailableDate, $nutritionistAvailableTime);
 $nutritionistsView->renderView();
 getBookingInformation();
