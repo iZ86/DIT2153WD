3e93f30501149f15a2369dc4b66224c9348e2bc7
skyfoojs <skyfoojs@gmail.com>
2024-10-28 22:23:32 +0800

    Add image file path constant

--- frontEnd/app/config/config.php
+++ frontEnd/app/config/config.php
@@ -5,3 +5,4 @@ define('SV_NAME', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'huanfitnesscentredatabase');
+define('IMAGE_FILE_PATH', 'http://localhost/DIT2153WD/frontEnd/app/public/images/');
