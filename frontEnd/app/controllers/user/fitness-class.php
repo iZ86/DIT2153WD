2e5ad1669bed7772718561ca2196aaf11fa25236
skyfoojs <skyfoojs@gmail.com>
2024-10-30 01:55:08 +0800

    Add validations and add TODO

--- frontEnd/app/controllers/user/fitness-class.php
+++ frontEnd/app/controllers/user/fitness-class.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require '../../models/user/userFitnessClassModel.php';
 require '../../views/user/pages/classScheduleView.php';
 
@@ -7,7 +8,18 @@ $weekOffset = isset($_GET['week']) ? intval($_GET['week']) : 0;
 $fitnessClassID = isset($_GET['fitnessClassID']) ? intval($_GET['fitnessClassID']) : null;
 
 $fitnessClassModel = new UserFitnessClass(require '../../config/db_connection.php');
+$scheduledOn = null;
+$userID = isset($_SESSION['userID']) ? $_SESSION['userID'] : null;
 
+if(isset($_POST['confirm-fitness-class-booking'])) {
+    if(isset($_POST['scheduledOn'])){
+        $scheduledOn = $_POST['scheduledOn'];
+    }
+}
+
+$fitnessClassScheduleID = $fitnessClassModel->getFitnessClassScheduleIdByClassInfo($scheduledOn, $instructorId, $fitnessClassID);
+$fitnessClassModel->createUserFitnessClassBooking($fitnessClassScheduleID, $userID);
+// TODO: Fix instructorID null bug.
 // Fetch instructor name and class data
 $instructorName = $fitnessClassModel->getInstructorNameById($instructorId);
 $classData = $fitnessClassModel->getClassesByInstructorById($instructorId);
