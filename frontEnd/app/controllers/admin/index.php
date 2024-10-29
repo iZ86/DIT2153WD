a757434d0d6b9990c220f74553d314ceded39b42
i4GS <admin@i4.gs>
2024-10-29 10:54:17 +0800

    Added verify admin session

--- frontEnd/app/controllers/admin/index.php
+++ frontEnd/app/controllers/admin/index.php
@@ -3,12 +3,18 @@ require '../../views/admin/pages/adminIndexView.php';
 require '../../models/admin/adminIndexModel.php';
 session_start();
 
+if (!isset($_SESSION['adminID'])) {
+    header("Location: ../../controllers/login.php");
+    exit;
+}
+
 $adminIndexModel = new AdminIndexModel(require '../../config/db_connection.php');
 
 $usersCount = $adminIndexModel->countUsers();
 $classesCount = $adminIndexModel->countClasses();
 $nutritionistsCount = $adminIndexModel->countNutritionists();
 $instructorsCount = $adminIndexModel->countInstructors();
+
 
 $adminIndexView = new AdminIndexView($usersCount, $classesCount, $nutritionistsCount, $instructorsCount);
 $adminIndexView->renderView();
