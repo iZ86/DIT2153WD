f27b911ebfa7e5e9cb1ce0375a2b4f47d10f8d3e
i4GS <admin@i4.gs>
2024-10-29 11:07:36 +0800

    Added and modify sql function for page pagination

--- frontEnd/app/models/admin/adminUsersModel.php
+++ frontEnd/app/models/admin/adminUsersModel.php
@@ -9,44 +9,35 @@ class AdminUsersModel {
         $this->databaseConn = $databaseConn;
     }
 
-    public function getAllUsers() {
+    public function getAllUsers($limit, $offset) {
         $query = "SELECT ru.registeredUserID, ru.firstName, ru.lastName, CONCAT(ru.firstName, ' ', ru.lastName) AS fullName, 
-                     ru.phoneNo, ru.email, ru.gender, ru.dateOfBirth,
+                     ru.phoneNo, ru.username, ru.email, ru.gender, ru.dateOfBirth,
                      IF(ms.endOn IS NULL OR ms.endOn < NOW(), 'Inactive', 'Active') AS membershipStatus 
               FROM " . $this->registeredUserTable . " AS ru
               LEFT JOIN " . $this->userTable . " AS u ON ru.registeredUserID = u.userID
-              LEFT JOIN " . $this->memberSubscriptionTable . " AS ms ON ms.membershipID = u.userID";
+              LEFT JOIN " . $this->memberSubscriptionTable . " AS ms ON ms.membershipID = u.userID
+              LIMIT ? OFFSET ?";
 
         $stmt = $this->databaseConn->prepare($query);
+        $stmt->bind_param("ii", $limit, $offset);
         $stmt->execute();
         return $stmt->get_result();
     }
 
-    public function addUser($firstName, $lastName, $username, $email, $phoneNo, $gender, $dateOfBirth) {
-        $registerUserQuery = "INSERT INTO " . $this->registeredUserTable . " 
-                              (firstName, lastName, username, email, phoneNo, gender, dateOfBirth)
-                              VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $this->databaseConn->prepare($registerUserQuery);
-        $stmt->bind_param("sssssss", $firstName, $lastName, $username, $email, $phoneNo, $gender, $dateOfBirth);
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to add user: " . $stmt->error);
-        }
-
-        $userID = $this->databaseConn->insert_id;
-        $addUserQuery = "INSERT INTO " . $this->userTable . " (userID, joinedDate) VALUES (?, NOW())";
-        $userStmt = $this->databaseConn->prepare($addUserQuery);
-        $userStmt->bind_param("i", $userID);
-        if (!$userStmt->execute()) {
-            throw new Exception("Failed to add user to USER table: " . $userStmt->error);
-        }
+    public function getUserCount() {
+        $query = "SELECT COUNT(*) as total FROM " . $this->registeredUserTable;
+        $stmt = $this->databaseConn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc()['total'];
     }
 
-    public function editUser($registeredUserID, $firstName, $lastName, $email, $phoneNo, $gender, $dateOfBirth) {
+    public function editUser($registeredUserID, $firstName, $lastName, $username, $email, $phoneNo, $gender, $dateOfBirth) {
         $updateQuery = "UPDATE " . $this->registeredUserTable . " 
-                        SET firstName = ?, lastName = ?, email = ?, phoneNo = ?, gender = ?, dateOfBirth = ?
+                        SET firstName = ?, lastName = ?, username = ?, email = ?, phoneNo = ?, gender = ?, dateOfBirth = ?
                         WHERE registeredUserID = ?";
         $stmt = $this->databaseConn->prepare($updateQuery);
-        $stmt->bind_param("ssssssi", $firstName, $lastName, $email, $phoneNo, $gender, $dateOfBirth, $registeredUserID);
+        $stmt->bind_param("sssssssi", $firstName, $lastName, $username, $email, $phoneNo, $gender, $dateOfBirth, $registeredUserID);
         if (!$stmt->execute()) {
             throw new Exception("Failed to update user: " . $stmt->error);
         }
