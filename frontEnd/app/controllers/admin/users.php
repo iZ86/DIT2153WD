ebf78740a0bcceb25613d5bea1381de6b802f25c
i4GS <admin@i4.gs>
2024-10-29 11:03:57 +0800

    Added verify admin session and page pagination

--- frontEnd/app/controllers/admin/users.php
+++ frontEnd/app/controllers/admin/users.php
@@ -3,34 +3,28 @@ require '../../views/admin/pages/adminUsersView.php';
 require '../../models/admin/adminUsersModel.php';
 session_start();
 
+if (!isset($_SESSION['adminID'])) {
+    header("Location: ../../controllers/login.php");
+    exit;
+}
+
 $adminUsersModel = new AdminUsersModel(require '../../config/db_connection.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
-    if (isset($_POST['addUserButton']) && $_POST['addUserButton'] === "Add User") {
-        $firstName = trim($_POST['firstName']);
-        $lastName = trim($_POST['lastName']);
-        $username = trim($_POST['username']);
-        $email = trim($_POST['email']);
-        $phoneNo = trim($_POST['phoneNo']);
-        $gender = trim($_POST['gender']);
-        $dateOfBirth = trim($_POST['dateOfBirth']);
-
-        if (empty($errors)) {
-            $adminUsersModel->addUser($firstName, $lastName, $username, $email, $phoneNo, $gender, $dateOfBirth);
-            header("Location: " . $_SERVER['PHP_SELF']);
-            exit;
-        }
-    }
-
     if (isset($_POST['editUserButton']) && $_POST['editUserButton'] === "Edit User") {
         $registeredUserID = $_POST['registeredUserID'];
-        $adminUsersModel->editUser($registeredUserID, $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['phoneNo'], $_POST['gender'], $_POST['dateOfBirth']);
+        $adminUsersModel->editUser($registeredUserID, $_POST['firstName'], $_POST['lastName'], $_POST['username'], $_POST['email'], $_POST['phoneNo'], $_POST['gender'], $_POST['dateOfBirth']);
         header("Location: " . $_SERVER['PHP_SELF']);
         exit;
     }
 }
 
-$users = $adminUsersModel->getAllUsers();
-$adminUsersView = new AdminUsersView($users);
+$limit = 6;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($page - 1) * $limit;
+
+$totalUsers = $adminUsersModel->getUserCount();
+$users = $adminUsersModel->getAllUsers($limit, $offset);
+$totalPages = ceil($totalUsers / $limit);
+$adminUsersView = new AdminUsersView($users, $totalPages, $page);
 $adminUsersView->renderView();
