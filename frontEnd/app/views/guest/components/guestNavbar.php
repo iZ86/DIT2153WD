9b0ed7962e55ecaec8536c50b439dd988cab162f
Lani <lanishaanusri28@gmail.com>
2024-10-29 15:03:27 +0800

    fixed guest navbar

--- frontEnd/app/views/guest/components/guestNavbar.php
+++ frontEnd/app/views/guest/components/guestNavbar.php
@@ -3,8 +3,8 @@
         <img class="w-40 h-20" src="../public/images/Logo.png" alt="Logo.png">
 
         <div class="flex font-bold space-x-14 text-lg">
-            <a href="../controllers/index.php" class="my-1">Home</a>
-            <a href="../controllers/login.php" class="bg-orange-500 rounded-full px-4 py-1 text-white">Login</a>
+            <a href="http://localhost/DIT2153WD/frontEnd/app/controllers/" class="my-1">Home</a>
+            <a href="http://localhost/DIT2153WD/frontEnd/app/controllers/login.php" class="bg-orange-500 rounded-full px-4 py-1 text-white">Login</a>
         </div>
     </div>
 </nav>
