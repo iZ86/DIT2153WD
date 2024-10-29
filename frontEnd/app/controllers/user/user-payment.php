8138b5973a7ae5ee16e405fcc1366b0507133632
skyfoojs <skyfoojs@gmail.com>
2024-10-29 14:37:29 +0800

    Add some validation

--- frontEnd/app/controllers/user/user-payment.php
+++ frontEnd/app/controllers/user/user-payment.php
@@ -7,6 +7,11 @@ $userPaymentModel = new UserPaymentModel(require '../../config/db_connection.php
 // Fetch classes data.
 $userPaymentData = $userPaymentModel->getAllUserPaymentInformation();
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['subscribe'])) {
+
+    }
+}
 // Pass the data to the view
 $paymentView = new PaymentView($userPaymentData);
 $paymentView->renderView();
