9918f3bd899bcb363516ff534a94b429a425bb36
skyfoojs <skyfoojs@gmail.com>
2024-10-30 15:38:16 +0800

    Fix insert booking if its null

--- frontEnd/app/controllers/user/fitness-class.php
+++ frontEnd/app/controllers/user/fitness-class.php
@@ -11,23 +11,23 @@ $instructorIdForPost = null;
 $fitnessClassID = null;
 $userID = isset($_SESSION['userID']) ? $_SESSION['userID'] : null;
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if(isset($_POST['confirm-fitness-class-booking'])) {
-        if(isset($_POST['scheduledOn']) && isset($_POST['instructorID']) && isset($_POST['fitnessClassID'])){
-            $scheduledOn = $_POST['scheduledOn'];
-            $instructorIdForPost = $_POST['instructorID'];
-            $fitnessClassID = $_POST['fitnessClassID'];
-        }
-    }
-}
-
-$fitnessClassScheduleID = $fitnessClassModel->getFitnessClassScheduleIdByClassInfo($scheduledOn, $instructorIdForPost, $fitnessClassID);
-$fitnessClassModel->createUserFitnessClassBooking($fitnessClassScheduleID, $userID);
-
 // Fetch instructor name and class data
 $instructorName = $fitnessClassModel->getInstructorNameById($instructorIdForOffSet);
 $classData = $fitnessClassModel->getClassesByInstructorById($instructorIdForOffSet);
 
 // Pass the data to the view
 $fitnessClassView = new FitnessClassView($classData, $instructorName);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_POST['confirm-fitness-class-booking'])) {
+        if(isset($_POST['scheduledOn']) && isset($_POST['instructorID']) && isset($_POST['fitnessClassID'])){
+            $scheduledOn = $_POST['scheduledOn'];
+            $instructorIdForPost = $_POST['instructorID'];
+            $fitnessClassID = $_POST['fitnessClassID'];
+            $fitnessClassScheduleID = $fitnessClassModel->getFitnessClassScheduleIdByClassInfo($scheduledOn, $instructorIdForPost, $fitnessClassID);
+            $fitnessClassModel->createUserFitnessClassBooking($fitnessClassScheduleID, $userID);
+        }
+    }
+}
+
 $fitnessClassView->renderView();
