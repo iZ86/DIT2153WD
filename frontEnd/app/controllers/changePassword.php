2aec1fb2b5e4df650bd7f02d8902a8b24a57ce04
l4n1skyy <lanishaanusri28@gmail.com>
2024-10-31 11:49:45 +0800

    change pass, forgot pass

--- frontEnd/app/controllers/changePassword.php
+++ frontEnd/app/controllers/changePassword.php
@@ -2,32 +2,50 @@
 require "../views/guest/pages/guestChangePasswordView.php";
 require "../models/guest/guestChangePasswordModel.php";
 session_start();
-$guestForgotPasswordModel = new GuestChangePasswordModel(require "../config/db_connection.php");
+$guestChangePasswordModel = new GuestChangePasswordModel(require "../config/db_connection.php");
 
-if (isset($_POST['changePassword']) && $_POST['changePassword'] === "Change Password") {
-    if ($_POST['newPassword'] === $_POST['newPasswordRepeat']) {
-        $password = $_POST['newPassword'];
-        $guestForgotPasswordModel->changePassword($_SESSION["changePasswordEmail"], $password);
-        unsetSessionVariablesForSelf();
-        die(header("location: http://localhost/DIT2153WD/frontEnd/app/controllers/login.php"));
-    } else {
-        $_SESSION['invalidChangePassword'] = 1;
-    }
-}
-
+// Only render the page if token is corrent
 if (isset($_GET['token'])) {
+    // get token from url
     $token = $_GET['token'];
-    $email = $guestForgotPasswordModel->verifyToken($token);
-    if (isset($email)) {
-        $_SESSION['changePasswordEmail'] = $email;
-        $guestChangePasswordView = new GuestChangePasswordView();
-        $guestChangePasswordView->renderView();
-        unsetSessionVariablesForSelf();
-    }
-}
+    $_SESSION['changePasswordToken'] = $token;
 
-/** Unsets all the session variables that is used by this controller only. */ 
-function unsetSessionVariablesForSelf() {
-    unset($_SESSION['changePasswordEmail']);
+    // Error Checking: Verify the token, redirect to forgot password if token is fake
+    $email = $guestChangePasswordModel->verifyToken($token);
+    if (!$email) {
+        $_SESSION["changePasswordError"] = 1;
+        die(header("location: ../controllers/forgotPassword.php"));
+    }
+
+    // If user click change password button, update the password in the database
+    if (isset($_POST['changePassword']) && $_POST['changePassword'] === "Change Password") {
+
+        // Error Checking: Check for empty fields
+        if (empty($_POST['newPassword']) || empty($_POST['newPasswordRepeat'])) {
+            $_SESSION['changePasswordError'] = 2;
+            die(header("location: ". $_SERVER['PHP_SELF'] . "?token=" . $token));
+        }
+
+        // Error Checking: Check if passwords match
+        if ($_POST['newPassword'] !== $_POST['newPasswordRepeat']) {
+            $_SESSION['changePasswordError'] = 3;
+            die(header("location: ". $_SERVER['PHP_SELF'] . "?token=" . $token));
+        }
+
+        $guestChangePasswordModel->changePassword($email, $_POST["newPassword"]);
+        $_SESSION['changePasswordSuccess'] = 1;
+        die(header("location: ". $_SERVER['PHP_SELF'] . "?token=" . $token));
+
+    }
+
+    $guestChangePasswordView = new GuestChangePasswordView();
+    $guestChangePasswordView->renderView();
+    /** Unsets all the session variables that is used by this controller only. */
+    unset($_SESSION['changePasswordToken']);
+    unset($_SESSION['changePasswordError']);
+    unset($_SESSION['changePasswordSuccess']);
+} else {
+    // redirect to forgot password if there is no token in url
+    die(header("location: ../controllers/forgotPassword.php"));
 }
 
