6f4f82d416d970f7dfebc0961a606260a72ca9bb
iZ86 <iZLikesCS@gmail.com>
2024-10-31 12:50:45 +0800

    Modify renderOneWaterConsumptionDataRow(1arg)
    Rename from $waterConsumptionDataID to $waterConsumptionID

--- frontEnd/app/views/user/pages/userTrackWaterConsumptionView.php
+++ frontEnd/app/views/user/pages/userTrackWaterConsumptionView.php
@@ -36,11 +36,11 @@ class UserTrackWaterConsumptionView {
     }
 
     /** Renders ONE card of water consumption data. */
-    private function renderOneWaterConsumptionDataRow($waterConsumptionDataID) {?>
-    <div id=<?php echo $waterConsumptionDataID; ?> class="basis-32 bg-blue-vivid flex items-center border-b-2 border-gray-mid shrink-0 hover:bg-blue-mid cursor-pointer" onclick="openEditWaterConsumptionDataModal(<?php echo $waterConsumptionDataID . ')';?>">
+    private function renderOneWaterConsumptionDataRow($waterConsumptionID) {?>
+    <div id=<?php echo $waterConsumptionID; ?> class="basis-32 bg-blue-vivid flex items-center border-b-2 border-gray-mid shrink-0 hover:bg-blue-mid cursor-pointer" onclick="openEditWaterConsumptionDataModal(<?php echo $waterConsumptionID . ')';?>">
         <img src="../../public/images/track_water_consumption_icon.png" class="w-16 h-16 mx-8">
         <div class="flex-col">
-            <p id=<?php echo '"' . $waterConsumptionDataID . 'Text"'; ?> class="mb-0 text-white font-bold text-lg drop-shadow-dark"></p>
+            <p id=<?php echo '"' . $waterConsumptionID . 'Text"'; ?> class="mb-0 text-white font-bold text-lg drop-shadow-dark"></p>
         </div>
     </div>
     <?php
