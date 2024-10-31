eec7df2047f5b3be9b467bd3dfd47c82b4e2996a
iZ86 <iZLikesCS@gmail.com>
2024-11-01 01:04:18 +0800

    Remove Cancel Plan button
    Not going to implement it

--- frontEnd/app/views/user/pages/userMembershipView.php
+++ frontEnd/app/views/user/pages/userMembershipView.php
@@ -39,18 +39,16 @@ class UserMembershipView {
 
     /** Renders the content to display membership status if membership is active. */
     public function renderMembershipActiveStatusContent() {?>
-    <div class="flex bg-orange-500 text-white rounded-2xl shadow-lg mt-10 max-w-192 min-h-80">
+    <div class="flex bg-orange-500 text-white rounded-2xl shadow-lg mt-10 min-w-192 min-h-80">
         <div class="flex-col justify-start basis-144">
             <img src="../../public/images/white_title.png" alt="HUAN white title" class="w-52">
-            <h1 class="text-4xl font-bold ml-5">Membership Active</h1>
-            <p class="text-lg font-semibold font-nunito ml-5"><?php echo $this->activeMembershipSubscriptionData["membershipType"] . " " . $this->activeMembershipSubscriptionData["membershipType"]?></p>
+            <h1 class="text-4xl font-bold ml-7">Membership Active</h1>
+            <p class="text-lg font-semibold font-nunito ml-7"><?php echo $this->activeMembershipSubscriptionData["membershipType"] . " " . $this->activeMembershipSubscriptionData["membershipType"]?></p>
             <div class="flex">
-                <p class="ml-5 basis-128">Your subscription will automatically renew on <b><?php echo $this->activeMembershipSubscriptionData["endOn"]?></b>
-                and you'll be charged <b><?php echo "RM" . $this->totalPrice?></b></p>
+                <p class="ml-7 basis-128">Your subscription will expire on <b><?php echo $this->activeMembershipSubscriptionData["endOn"]?></b>.</p>
             </div>
-            <div class="flex">
-                <div class="flex max-h-12 my-10">
-                    <button class="bg-white text-orange-500 px-6 py-3 font-semibold rounded-3xl hover:bg-gray-200 text-base font-nunito ml-5 mr-4">Cancel Plan</button>
+            <div class="flex items-end">
+                <div class="flex max-h-12 ml-7 mt-16">
                     <button class="bg-white text-orange-500 px-6 py-3 font-semibold rounded-3xl hover:bg-gray-200 text-base font-nunito mr-4">View Current Plan</button>
                     <button class="bg-white text-orange-500 px-6 py-3 font-semibold rounded-3xl hover:bg-gray-200 text-base font-nunito">Switch Plans</button>
                 </div>
@@ -66,17 +64,17 @@ class UserMembershipView {
 
     /** Renders the content to display membership status if membership is inactive. */
     public function renderMembershipInactiveStatusContent() {?>
-    <div class="flex bg-gray-500 text-white rounded-2xl shadow-lg mt-10 max-w-192 min-h-80">
+    <div class="flex bg-gray-500 text-white rounded-2xl shadow-lg mt-10 min-w-192 min-h-80">
         <div class="flex-col justify-start basis-144">
             <img src="../../public/images/white_title.png" alt="HUAN white title" class="w-52">
-            <h1 class="text-4xl font-bold ml-5">Membership Inactive</h1>
+            <h1 class="text-4xl font-bold ml-7">Membership Inactive</h1>
             <div class="flex">
-                <p class="ml-5 basis-128">Get incredible rich features to enhance your experience, with membership, 
+                <p class="ml-7 max-w-128">Get incredible rich features to enhance your experience, with membership, 
                 cancel anytime, starting at only RM50!</p>
             </div>
-            <div class="flex">
-                <div class="flex max-h-12 mt-16">
-                    <button class="bg-orange-500 text-white px-6 py-3 font-semibold rounded-3xl hover:bg-orange-700 text-base font-nunito ml-5">View Current Plan</button>
+            <div class="flex items-end">
+                <div class="flex max-h-12 ml-7 mt-17">
+                    <button class="bg-orange-500 text-white px-6 py-3 font-semibold rounded-3xl hover:bg-orange-700 text-base font-nunito">View Current Plan</button>
                 </div>
                     
             </div>
