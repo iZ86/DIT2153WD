88c0f5b092831c7973efd57a6171398130ca157b
iZ86 <iZLikesCS@gmail.com>
2024-10-31 19:46:11 +0800

    Add functionality to user navbar

--- frontEnd/app/views/user/components/userNavbar.php
+++ frontEnd/app/views/user/components/userNavbar.php
@@ -1,13 +1,13 @@
 <nav class="z-40 relative">
+    <script src="../../public/js/user/userNavbarScript.js"></script>
     <div class="bg-white flex py-3 px-12 font-montserrat justify-between">
-        <img class="w-40 h-20" src="../../public/images/Logo.png" alt="Logo.png">
-
+        <img class="w-40 h-20 cursor-pointer" src="../../public/images/Logo.png" alt="Logo.png" onclick="redirectToIndex()">
+        <?php $currentPage = basename($_SERVER['PHP_SELF']);?>
         <div class="px-11 flex items-center gap-x-14">
             <div class="flex font-bold space-x-14 text-lg">
-                <a href="">Home</a>
+                <a href="index.php" class="<?php echo $currentPage === "index.php" ? "text-orange-400" : "text-black hover:text-orange-400"?>" >Home</a>
                 <a href="">Explore</a>
-                <a href="http://localhost/DIT2153WD/frontEnd/app/controllers/user/nutritionist.php">Nutritionists</a>
-                <a href="">Feedbacks</a>
+                <a href="nutritionist.php" class="<?php echo $currentPage === "nutritionist.php" ? "text-orange-400" : "text-black hover:text-orange-400"?>">Nutritionists</a>
             </div>
 
             <div class="flex justify-center items-center">
