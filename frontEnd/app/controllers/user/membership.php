2bf6227de287c806214defa8a1743019755bcf3a
iZ86 <iZLikesCS@gmail.com>
2024-11-01 00:09:55 +0800

    Completed membership page

---
+++ frontEnd/app/controllers/user/membership.php
@@ -0,0 +1,20 @@
+<?php
+require '../../views/user/pages/userMembershipView.php';
+require '../../models/user/userMembershipModel.php';
+session_start();
+$userMembershipModel = new UserMembershipModel(require '../../config/db_connection.php');
+$activeMembershipSubscriptionData = $userMembershipModel->getActiveMembershipSubscriptionData($_SESSION['userID']);
+// If the data is empty, there is no active membership subscription.
+if (sizeof($activeMembershipSubscriptionData) > 0) {
+    $sumPriceOfFitnessClassSubscription = $userMembershipModel->getSumPriceOfFitnessClassSubscriptionToPaymentID($activeMembershipSubscriptionData['paymentID']);
+    $totalPrice = $activeMembershipSubscriptionData['price'];
+    $totalPrice += sizeof($sumPriceOfFitnessClassSubscription) > 0 ? $sumPriceOfFitnessClassSubscription['sumPriceOfFitnessClassSubscriptionToPaymentID'] : 0;
+
+    $userMembershipView = new UserMembershipView($activeMembershipSubscriptionData, $totalPrice);
+    $userMembershipView->renderView();
+} else {
+    $userMembershipView = new UserMembershipView($activeMembershipSubscriptionData, 0);
+    $userMembershipView->renderView();
+
+}
+
