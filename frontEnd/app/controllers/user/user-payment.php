dc1842dc4debf9040f31a5e135c3c0fb91e63a0b
skyfoojs <skyfoojs@gmail.com>
2024-10-31 14:42:02 +0800

    Fix bug payment will still insert even though nutritionist is booked

--- frontEnd/app/controllers/user/user-payment.php
+++ frontEnd/app/controllers/user/user-payment.php
@@ -26,40 +26,47 @@ function checkIsBasicPostVariablesSet() {
     return false;
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(isset($_POST['subscribe'])) {
-        if($_POST['subscribe'] === "Subscribe") {
-            $email = cleanData($_POST['email']);
-            $firstName = cleanData($_POST['firstName']);
-            $lastName = cleanData($_POST['lastName']);
-            $address = cleanData($_POST['address']);
-            $country = cleanData($_POST['country']);
-            $zipCode = cleanData($_POST['zipCode']);
-            $country = cleanData($_POST['city']);
-            $state = cleanData($_POST['state']);
-            $phoneNumber = cleanData($_POST['phoneNumber']);
-            $type = isset($_GET['order']) ? cleanData($_GET['order']) : null;
-            $status = "Completed";
-            $createdOn = date('Y-m-d H:i:s');
-            $nutritionistBookingDescription = isset($_SESSION['description']) ? cleanData($_SESSION['description']) : null;
-            $nutritionistScheduleID = isset($_SESSION['nutritionistScheduleID']) ? cleanData($_SESSION['nutritionistScheduleID']) : null;
-            $paymentID = $userPaymentModel->getPaymentIDByTypeCreatedOnAndUserID($type, $createdOn, htmlspecialchars($_SESSION['userID']));
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['subscribe']) && $_POST['subscribe'] === "Subscribe") {
+        $email = cleanData($_POST['email']);
+        $firstName = cleanData($_POST['firstName']);
+        $lastName = cleanData($_POST['lastName']);
+        $address = cleanData($_POST['address']);
+        $country = cleanData($_POST['country']);
+        $zipCode = cleanData($_POST['zipCode']);
+        $city = cleanData($_POST['city']);
+        $state = cleanData($_POST['state']);
+        $phoneNumber = cleanData($_POST['phoneNumber']);
+        $type = isset($_GET['order']) ? cleanData($_GET['order']) : null;
+        $status = "Completed";
+        $createdOn = date('Y-m-d H:i:s');
+        $nutritionistBookingDescription = isset($_SESSION['description']) ? cleanData($_SESSION['description']) : null;
+        $nutritionistScheduleID = isset($_SESSION['nutritionistScheduleID']) ? cleanData($_SESSION['nutritionistScheduleID']) : null;
+        $userID = $_SESSION['userID'];
 
-                // Check if the schedule is already booked
-                if ($userPaymentModel->isScheduleIDBooked($nutritionistScheduleID)) {
-                    echo "<script>alert('This schedule is already booked.Please select a different schedule.'); window.location.href='http://localhost/DIT2153WD/frontEnd/app/controllers/user/nutritionist.php';</script>";
+            // Check if the schedule is already booked
+            if ($userPaymentModel->isScheduleIDBooked($nutritionistScheduleID)) {
+
+                echo "<script>alert('This schedule is already booked. Please select a different schedule.'); window.location.href='http://localhost/DIT2153WD/frontEnd/app/controllers/user/nutritionist.php';</script>";
+            } else {
+                // Insert payment record
+                $userPaymentModel->createUserPayment($type, $status, $createdOn, $userID);
+
+                // Retrieve the last inserted payment ID
+                $paymentIDResult = $userPaymentModel->getPaymentIDByTypeCreatedOnAndUserID($type, $createdOn, $userID);
+
+                // Create booking record with the retrieved payment ID
+                $bookingResult = $userPaymentModel->createNutritionistBooking($nutritionistBookingDescription, $nutritionistScheduleID, $userID, $paymentID);
+
+                if ($bookingResult === true) {
+                    echo "<script>alert('Successfully booked the Nutritionist. Please make sure to be on time!'); window.location.href='http://localhost/DIT2153WD/frontEnd/app/controllers/user/nutritionist.php';</script>";
                 } else {
-                    $userPaymentModel->createUserPayment($type, $status, $createdOn, $_SESSION['userID']);
-                    $bookingResult = $userPaymentModel->createNutritionistBooking($nutritionistBookingDescription, $nutritionistScheduleID, $_SESSION['userID'], $paymentID['paymentID']);
-                    if ($bookingResult === true) {
-                        echo "<script>alert('Succesfully booked the Nutritionist. Please make sure be on time!'); window.location.href='http://localhost/DIT2153WD/frontEnd/app/controllers/user/nutritionist.php';</script>";
-                    } else {
-                        echo "<script>alert('Error: " . htmlspecialchars($bookingResult) . "');</script>";
-                    }
+                    echo "<script>alert('Error: " . htmlspecialchars($bookingResult) . "');</script>";
                 }
-        }
+            }
     }
 }
+
 // Pass the data to the view
 $paymentView = new PaymentView($userPaymentData);
 $paymentView->renderView();
