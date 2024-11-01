8f69443b9a4e16c234637546bfb85ad1d6bddf75
i4GS <admin@i4.gs>
2024-11-01 17:27:55 +0800

    Added DIR path of notification components in admin classes view

--- frontEnd/app/views/admin/pages/adminClassesView.php
+++ frontEnd/app/views/admin/pages/adminClassesView.php
@@ -63,6 +63,8 @@ class AdminClassesView {
 
         <section class="p-6 space-y-6">
             <div class="mx-4">
+                <?php include __DIR__ . '/../components/adminSuccessNotification.php'; ?>
+
                 <div class="flex items-center justify-between">
                     <h2 class="text-2xl font-bold">Classes</h2>
                     <div class="flex items-center space-x-4">
